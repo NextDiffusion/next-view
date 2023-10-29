a4149a424aabb8829b8dd48acedf12bac46a22d4
Next Diffusion <nextdiffusionai@gmail.com>
2023-10-29 21:11:31 +0100

    Handle missing frames and use temp file for ffmpeg

--- scripts/next_view.py
+++ scripts/next_view.py
@@ -5,12 +5,16 @@ from modules import script_callbacks
 import modules.scripts as scripts
 import datetime
 import re
+import tempfile
 
 base_dir = scripts.basedir()
 
 
 def split_video_to_images(video_path, output_dir):
+    # Define the pattern for naming the output frames
     output_pattern = Path(output_dir) / "frame_%04d.png"
+
+    # Use ffprobe to get the frame rate of the input video
     ffprobe_cmd = [
         "ffprobe",
         "-v", "error",
@@ -24,10 +28,11 @@ def split_video_to_images(video_path, output_dir):
     frame_rate = eval(result.stdout)
     print(f"Frame rate of input video: {frame_rate} fps")
 
+    # Use ffmpeg to split the video into image sequences with the determined frame rate
     ffmpeg_cmd = [
         "ffmpeg",
         "-i", video_path,
-        "-vf", f"fps={frame_rate}",  # Use the determined frame rate
+        "-vf", f"fps={frame_rate}",
         output_pattern,
     ]
     subprocess.run(ffmpeg_cmd)
@@ -35,6 +40,7 @@ def split_video_to_images(video_path, output_dir):
 
 
 def submit_video(video):
+    # Convert the video path to a pathlib.Path object
     video_directory = Path(video)
     print(f"Uploaded video directory: {video_directory}")
 
@@ -46,51 +52,60 @@ def submit_video(video):
     # Create parent directories if needed
     output_dir.mkdir(parents=True, exist_ok=True)
 
+    # Split the uploaded video into image sequences
     split_video_to_images(video, output_dir)
     return str(output_dir)  # Return the output directory as a string
 
 
 def image_sequence_to_video(image_sequence_location, fps):
-    image_sequence_location = Path(image_sequence_location)
+    # Convert the image sequence location to an absolute path
+    image_sequence_location = Path(image_sequence_location).absolute()
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     output_directory = Path(base_dir, "output_videos")
-    # Create the directory if it doesn't exist
     output_directory.mkdir(parents=True, exist_ok=True)
+
+    frame_files = sorted(image_sequence_location.glob(
+        "frame_*.png"), key=lambda x: int(re.search(r'(\d+)', x.name).group()))
+    frame_numbers = [int(re.search(r'(\d+)', frame.name).group())
+                     for frame in frame_files]
+
+    # Explicitly create a file list with correct order
+    file_list_path = Path(base_dir, "file_list.txt")
+    with file_list_path.open('w') as file_list:
+        for num in frame_numbers:
+            file_list.write(
+                f"file '{image_sequence_location}/frame_{num:04d}.png'\n")
+
+    # Use ffmpeg with the file list to generate the output video
     output_video_path = output_directory / f"output_video_{timestamp}.mp4"
-
-    # Detect the first frame number dynamically
-    frame_files = sorted(image_sequence_location.glob("frame_*.png"))
-    if frame_files:
-        first_frame = frame_files[0].name
-        match = re.search(r'(\d+)', first_frame)
-        if match:
-            start_frame_number = int(match.group())
-        else:
-            start_frame_number = 1
-    else:
-        start_frame_number = 1
-
-    # Use ffmpeg to convert image sequences into a video
     ffmpeg_cmd = [
         "ffmpeg",
-        "-framerate", f"{fps}",  # You can set the desired frame rate here
-        "-start_number", str(start_frame_number),  # Set the start frame number
-        "-i", f"{image_sequence_location}/frame_%04d.png",
+        "-r", f"{fps}",
+        "-f", "concat",
+        "-safe", "0",
+        "-i", str(file_list_path),
         "-c:v", "libx264",
         "-pix_fmt", "yuv420p",
-        output_video_path,
+        str(output_video_path),
     ]
+
     subprocess.run(ffmpeg_cmd)
 
-    return output_video_path
+    # Remove the temporary file list
+    file_list_path.unlink()
+
+    print(f"Video generated at: {output_video_path}")
+
+    return str(output_video_path)
 
 
 def on_ui_tabs():
-
+    # Define the UI layout using Gradio Blocks
     with gr.Blocks(analytics_enabled=False) as next_view:
         with gr.Row():
             with gr.Column():
                 gr.HTML('''<h2>Video 2 Image Sequence 👇</h2>''')
+                # Define the input components for uploading a video
                 inp = gr.Video(
                     type="file",
                     format="mp4",
@@ -101,25 +116,30 @@ def on_ui_tabs():
                 )
                 with gr.Row():
                     gr.ClearButton(inp)
+                    # Define the button for generating image sequences
                     btn = gr.Button("Generate Image Sequence",
                                     elem_id="submit_video_button")
 
+                # Define the output component for displaying the image sequence location
                 out_location = gr.Textbox(
                     show_copy_button=True,
                     type="text",
                     label="Image Sequence Location"
                 )
 
+                # Set the click function for the "Generate Image Sequence" button
                 btn.click(fn=submit_video, inputs=inp, outputs=out_location)
 
             with gr.Column():
                 gr.HTML('''<h2>Image Sequence 2 Video 👇</h2>''')
                 with gr.Row():
+                    # Define the input component for specifying the image sequence location
                     inp = gr.Textbox(
                         type="text",
                         label="Image Sequence Location",
                     )
 
+                # Define the output component for displaying the generated video
                 out = gr.Video(
                     type="auto",
                     label="Generated Video",
@@ -127,15 +147,19 @@ def on_ui_tabs():
                     height=300,
                 )
 
+                # Define the slider for selecting frames per second (FPS)
                 fps = gr.Slider(2, 240, value=24, label="Frames Per Second (FPS)",
                                 step=1, info="Choose your FPS", elem_id="fps_slider")
+                # Define the button for generating the video
                 btn = gr.Button("Generate Video",
                                 elem_id="generate_video_button")
 
+                # Set the click function for the "Generate Video" button
                 btn.click(fn=image_sequence_to_video,
                           inputs=[inp, fps], outputs=out)
 
     return (next_view, "NextView", "NextView"),
 
 
+# Register the UI layout function with script_callbacks
 script_callbacks.on_ui_tabs(on_ui_tabs)
